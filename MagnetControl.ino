6b409988bb38d67f5578838807575c19bb4223f8
Felix <felix.f.klose@gmail.com>
2022-09-01 15:34:42 -0700

    Annotated code and got rid of some dead functions

--- MagnetControl.ino
+++ MagnetControl.ino
@@ -2,8 +2,19 @@
 #define DAC_UPDATE 8 // DAC update interval [us]
 #define PERIOD 4000 // Period of control cycle [us]
 #define TRAP_TIME 2500 // Time for trapping [us]
-#define THRESHOLD 0.1 // Triggering threshold for ADC
+#define THRESHOLD 0.1 // Triggering threshold for ADC (i.e. trigger when ADC input crosses this threshold)
 #define OP_DELAY 500 // Time delay between trapping field and OP field [us]
+#define OP_TIME 1000 // Timelength of optical pumping [us]
+
+// Set magnetic fields
+#define B_TRAP 60.0 // [G]
+#define B_OP 2.0 // [G]
+
+// Compute output voltages
+// Matsusada Supplies CC: 7.8 A/V
+// TRINAT Coil parameters: 1.11 G/A
+const double v_trap = B_TRAP / 1.11 / 7.8;
+const double v_op = B_OP / 1.11 / 7.8;
 
 // Common objects
 IntervalTimer Timer;
@@ -16,18 +27,19 @@ float bottom;
 void updateDAC() {
 	if (time < PERIOD) {
 		
-		// Trapping field ~50 A
-		out = cosPower(0, TRAP_TIME, 7, 8);
+		// Trapping field
+		out = cosPower(0, TRAP_TIME, v_trap);
 		
 		// Optical pumping field
-		top = out + cosPower(TRAP_TIME + OP_DELAY, 1000, 0.256, 8);
-		bottom = out - cosPower(TRAP_TIME + OP_DELAY, 1000, 0.256, 8);
+		top = out + cosPower(TRAP_TIME + OP_DELAY, OP_TIME, v_op);
+		bottom = out - cosPower(TRAP_TIME + OP_DELAY, OP_TIME, v_op);
 		
 		writeDAC1(top);
 		writeDAC2(bottom);
 
 		time += DAC_UPDATE;
 	} else {
+		// After completing one cycle end Timer
 		time = 0;
         Timer.end();
 	}
@@ -37,9 +49,12 @@ void updateDAC() {
 bool state = false;
 // ADC interrupt
 // Tells adc to start cycle when input goes above some threshold voltage
+// I.e. trigger Quarto on rising edge of some signal
 void ADC_interrupt() {
     float V = readADC1_from_ISR();
     if ((V > THRESHOLD) & !state) {
+		// Voltage crossed threshold and we were not in on state before
+		// so we start updating the DAC using an interrupt timer.
         Timer.begin(updateDAC, DAC_UPDATE);
         state = true;
     } else if ((V < THRESHOLD) & state) {
@@ -48,20 +63,11 @@ void ADC_interrupt() {
 }
 
 // Waveforms
-float RAC_sinusoid(int trap_time, float amplitude, int offset) {
-	float out;
-	if (time < trap_time) {
-		out = amplitude * sin(2*PI * time / PERIOD) + offset;
-	} else {
-		out = 0;
-	}
-	return out;
-}
-
-float cosPower(int start_time, int length, float amplitude, int power) {
+float cosPower(int start_time, int length, float amplitude) {
 	float out;
 	if ((time >= start_time) && (time - start_time < length)) {
-		// out = - amplitude * pow(cos(PI * (time - start_time) / length), power) + amplitude;
+		// The commented out code below produces some weird spikes in the output.
+		// out = - amplitude * pow(cos(PI * (time - start_time) / length), 8) + amplitude;
 		float x = cos(PI * (time - start_time) / length);
 		out = - amplitude * x * x * x * x * x * x * x * x + amplitude;
 	} else {
@@ -71,6 +77,7 @@ float cosPower(int start_time, int length, float amplitude, int power) {
 }
 
 void setup() {
+	// ADC value gets read out every 1 us in ADC_interrupt function defined above
     configureADC(1, 1, 1, BIPOLAR_10V, ADC_interrupt);
 }
 
